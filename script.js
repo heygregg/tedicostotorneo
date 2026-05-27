e06b0d6b910e9da95f8261562bac9609c2a996b2
heygregg <greg.clini98@gmail.com>
2026-05-27 10:14:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -222,8 +222,10 @@ function renderScorers() {
       <div class="info">
         <div class="team">${s[0]}</div>
         <div class="sub">
-            ${players[s[0]]?.played ?? 0} partite giocate • 
-            1 gol ogni ${Math.round((players[s[0]]?.played ?? 0) * 60 / s[1])} min
+              ${players[s[0]]?.played ?? 0} partite giocate •
+              ${s[1] > 0 
+                ? `1 gol ogni ${Math.round((players[s[0]]?.played ?? 0) * 60 / s[1])} min` 
+                : 'nessun gol'}
         </div>
       </div>
 
